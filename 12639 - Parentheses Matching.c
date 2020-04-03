25689a4d4bdb161cfa77fa62bab7c4d03eb76a8e
Kai Wu <wuvincentck505@gmail.com>
2020-04-03 22:54:37 +0800

    Create 12639 - Parentheses Matching.c

---
+++ 12639 - Parentheses Matching.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 1100
+
+short parenthesesMatching(char*);
+
+int main() {
+	int n, count = 0;
+	char **T;
+	scanf("%d", &n); getchar();
+
+	T = (char**)malloc(n * sizeof(char*));
+
+	for (int i = 0; i < n; i++) {
+		T[i] = (char*)malloc(SIZE * sizeof(char));
+		fgets(T[i], SIZE, stdin);
+	}
+
+	for (int i = 0; i < n; i++) {
+		count += parenthesesMatching(T[i]) ? 1 : 0;
+	}
+	printf("%d\n", count);
+	return 0;
+}
+
+short parenthesesMatching(char* arr) {
+	int err = 0, len = -1;
+	char stack[SIZE];
+	for (int i = 0; arr[i] != '\n'; i++) {
+		if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{' || arr[i] == '<') {
+			len++;
+			stack[len] = arr[i];
+			stack[len + 1] = '\0';
+		}
+		else if ((arr[i] == ')' && stack[len] == '(') || (arr[i] == ']' && stack[len] == '[') || (arr[i] == '}' && stack[len] == '{') || (arr[i] == '>' && stack[len] == '<')) {
+			stack[len] = '\0';
+			len--;
+		}
+		else err = 1;
+	}
+	return err || len > -1 ? 0 : 1;
+}
